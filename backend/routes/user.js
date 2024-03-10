51699550b0ce656b3aa0c36891b43fc38408aee5
Gwenaëlle <111727414+cgwena@users.noreply.github.com>
2024-03-10 16:36:30 +0100

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,3 +1,184 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Users:
+ *       type: object
+ *       required:
+ *         - name
+ *         - email
+ *         - password
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: The name of the user
+ *         email:
+ *           type: email
+ *           description: the email of the user
+ *         password:
+ *           type: string
+ *           description: the password of the user
+ *         
+ *       example:
+ *         name: John Doe
+ *         email: johndoe@mail.com
+ *         password: password
+ * 
+ *  
+ *     
+ */
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ * /api/users/signup:
+ *   post:
+ *     summary: Create a new user
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Users'
+ *     responses:
+ *       201:
+ *         description: The created user.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Users'
+ *       401:
+ *          description: Unauthorized
+ *       500:
+ *         description: Some server error
+ *
+ */
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ * /api/users/login:
+ *   post:
+ *     summary: Login a user
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Users'
+ *     responses:
+ *       201:
+ *         description: The created user.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Users'
+ *       401:
+ *          description: Unauthorized
+ *       500:
+ *         description: Some server error
+ *
+ */
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ * /api/users:
+ *   get:
+ *     summary: Read all the users
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: The list of the users.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Users'
+ *       401:
+ *          description: Unauthorized
+ *       500:
+ *         description: Some server error
+ *
+ */
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ * /api/users/id:
+ *   get:
+ *     summary: get one catway
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: One user's details.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Users'
+ *       401:
+ *          description: Unauthorized
+ *       500:
+ *         description: Some server error
+ *
+ */
+/**
+ * @swagger
+ * tags:
+ *   name: Catways
+ * /api/users/id:
+ *   put:
+ *     summary: update a user
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Users'
+ *     responses:
+ *       201:
+ *         description: The updated user.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Users'
+ *       401:
+ *          description: Unauthorized
+ *       500:
+ *         description: Some server error
+ *
+ */
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ * /api/users/id:
+ *   delete:
+ *     summary: Delete a user
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Users'
+ *     responses:
+ *       200:
+ *         description: The deleted user.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Users'
+ *       401:
+ *          description: Unauthorized
+ *       500:
+ *         description: Some server error
+ *
+ */
+
+
 const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/users')
