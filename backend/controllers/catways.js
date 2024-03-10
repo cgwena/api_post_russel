34e48331e3d93a9df72b89d88466a057a60e2adb
Gwenaëlle <111727414+cgwena@users.noreply.github.com>
2024-03-10 17:12:57 +0100

    Update catways.js

--- backend/controllers/catways.js
+++ backend/controllers/catways.js
@@ -1,5 +1,12 @@
 const Catway = require('../models/catways')
 
+/**
+ * Crée un nouveau catway.
+ * @param {object} req - L'objet de requête HTTP.
+ * @param {object} res - L'objet de réponse HTTP.
+ * @returns {object} - Le catway créé
+ */
+
 exports.createCatway = (req, res, next) => {
     const catway = new Catway({
         _id: req.body.catwayNumber,
@@ -12,11 +19,24 @@ exports.createCatway = (req, res, next) => {
         .catch(error => res.status(400).json({ error }));
 }
 
+/**
+ * Lit tous les catways.
+ * @param {object} req - L'objet de requête HTTP.
+ * @param {object} res - L'objet de réponse HTTP.
+ * @returns {object} - La liste des catways
+ */
 exports.readAllCatways = (req, res, next) => {
     return Catway.find()
         .then(catways => res.status(200).json(catways))
         .catch(error => res.status(400).json({ error }));
 }
+
+/**
+ * Lit un catway.
+ * @param {object} req - L'objet de requête HTTP.
+ * @param {object} res - L'objet de réponse HTTP.
+ * @returns {object} - Les détails d'un catway
+ */
 
 exports.readOneCatway = (req, res, next) => {
     Catway.findOne({ _id: req.params.id })
@@ -24,6 +44,12 @@ exports.readOneCatway = (req, res, next) => {
         .catch(error => res.status(404).json({ error }));
 }
 
+/**
+ * Met à jour un catway.
+ * @param {object} req - L'objet de requête HTTP.
+ * @param {object} res - L'objet de réponse HTTP.
+ * @returns {object} - Le catway mis à jour
+ */
 exports.updateCatway = (req, res, next) => {
     Catway.updateOne({ _id: req.params.id }, {
         catwayNumber: req.body.catwayNumber,
@@ -34,6 +60,13 @@ exports.updateCatway = (req, res, next) => {
         .then(() => res.status(200).json({ message: 'Objet modifié !' }))
         .catch(error => res.status(400).json({ error }));
 }
+
+/**
+ * Supprime un catway.
+ * @param {object} req - L'objet de requête HTTP.
+ * @param {object} res - L'objet de réponse HTTP.
+ * @returns {void}
+ */
 
 exports.deleteCatway = (req, res, next) => {
     Catway.deleteOne({ _id: req.params.id })
